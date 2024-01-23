a16cc9e9895d8ca23eab2851878a57553230828a
Parleigh <tshepo.parleighm@gmail.com>
2024-01-23 23:01:49 +0200

    Test102

--- next.config.js
+++ next.config.js
@@ -27,6 +27,11 @@ const nextConfig = {
         protocol: "https",
         hostname: "placehold.co",
       },
+      {
+        protocol: "https",
+        hostname: "utfs.io"
+      },
+
     ],
   },
 };
